be75ac0c38c26e9512e16f34f91dee0983b7d53e
Timotej Panták <timopantakgit@gmail.com>
2021-11-11 16:21:36 +0100

    Make Item interface and show it on main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
