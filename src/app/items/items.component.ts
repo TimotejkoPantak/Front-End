ac22c960e4d794e47f56298eff1fb257e5c37035
Timotej Panták <timopantakgit@gmail.com>
2021-11-15 18:31:35 +0100

    Make list of heroes, items and edit them

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from './item';
+import { ITEMS } from '../mock-items';
 
 @Component({
   selector: 'app-items',
@@ -8,15 +9,16 @@ import { Item } from './item';
 })
 export class ItemsComponent implements OnInit {
 
-  item: Item = {
-    id:1,
-    price:900,
-    name: "Frajerina"
-  };
-
+  items = ITEMS;
+  selectedItem?: Item;
+  onSelect(item: Item): void {
+    this.selectedItem = item;
+  }
   constructor() { }
+
 
   ngOnInit(): void {
   }
+
 
 }
