6a7999e08f7d4dbba846dc660d95418d159a581e
Timotej Panták <timopantakgit@gmail.com>
2021-11-17 10:40:09 +0100

    Sharing Data Between a Child and Parent Component (Heroes)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { ItemsComponent } from './items/items.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    ItemsComponent
+    ItemsComponent,
+    HeroDetailComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/item-detail/item-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../items/item';
+
+@Component({
+  selector: 'app-item-detail',
+  templateUrl: './item-detail.component.html',
+  styleUrls: ['./item-detail.component.css']
+})
+export class ItemDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
